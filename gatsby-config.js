d8ee134700d94c69d7473ca4865fba9743972906
Suisuikitai <mag.cupw@gmail.com>
2021-09-10 06:23:08 +0900

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,17 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "My Gatsby Site",
+    title: "My Gatsby Site"
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blolg`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 };
